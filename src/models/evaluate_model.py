85c0c8156f54358491ac348638a0533f5eedd19d
Hassan Ali <dilruba93r@gmail.com>
2025-05-21 11:44:57 +0500

    Update evaluate_model.py

--- src/models/evaluate_model.py
+++ src/models/evaluate_model.py
@@ -1,1 +1,18 @@
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+from torch.utils.data import DataLoader
+from transformers import ViTForImageClassification
+from sklearn.metrics import precision_score, recall_score, f1_score
+import numpy as np
+import matplotlib.pyplot as plt
+import shap
+import pandas as pd
 
+def shap_explain(model, dataloader):
+    model.eval()
+    batch = next(iter(dataloader))[0][:10].to(device)
+    explainer = shap.GradientExplainer(model, batch)
+    shap_values = explainer.shap_values(batch)
+    shap.image_plot(shap_values, batch.cpu().numpy())
