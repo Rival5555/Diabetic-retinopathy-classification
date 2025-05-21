31f102f0ab3afe3a58652833acba09c4a30a03f3
Hassan Ali <dilruba93r@gmail.com>
2025-05-21 11:46:53 +0500

    Update evaluate_model.py

--- src/models/evaluate_model.py
+++ src/models/evaluate_model.py
@@ -10,6 +10,40 @@ import matplotlib.pyplot as plt
 import shap
 import pandas as pd
 
+
+def test(model, val_loader):
+    model.eval()
+    correct, total = 0, 0
+    all_preds, all_labels = [], []
+    classification_results = []
+
+    with torch.no_grad():
+        for images, labels in val_loader:
+            images, labels = images.to(device, non_blocking=True), labels.to(device, non_blocking=True)
+            outputs = model(images).logits
+            predictions = outputs.argmax(1)
+            correct += (predictions == labels).sum().item()
+            total += labels.size(0)
+            all_preds.extend(predictions.cpu().numpy())
+            all_labels.extend(labels.cpu().numpy())
+
+            for i in range(len(labels)):
+                classification_results.append([labels[i].item(), predictions[i].item()])
+
+    accuracy = correct / total
+    f1 = f1_score(all_labels, all_preds, average='weighted')
+    precision = precision_score(all_labels, all_preds, average='weighted')
+    recall = recall_score(all_labels, all_preds, average='weighted')
+
+    print(f"Test Accuracy: {accuracy:.4f}")
+    print(f"Precision: {precision:.4f}, Recall: {recall:.4f}, F1-score: {f1:.4f}")
+
+    df_classification = pd.DataFrame(classification_results, columns=['Actual', 'Predicted'])
+    df_classification.to_csv("/content/classification_results.csv", index=False)
+
+    visualize_gradcam(model, val_loader)
+
+
 def shap_explain(model, dataloader):
     model.eval()
     batch = next(iter(dataloader))[0][:10].to(device)
