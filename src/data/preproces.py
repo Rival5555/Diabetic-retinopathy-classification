8a8cb223ca58aa8995ec9a9c1187c592954fc95b
Hassan Ali <dilruba93r@gmail.com>
2025-05-21 11:40:55 +0500

    Update preproces.py

--- src/data/preproces.py
+++ src/data/preproces.py
@@ -1,1 +1,19 @@
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+from torch.utils.data import DataLoader
+from transformers import ViTForImageClassification
+from sklearn.metrics import precision_score, recall_score, f1_score
+import numpy as np
+import matplotlib.pyplot as plt
+import shap
+import pandas as pd
 
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(10),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+])
